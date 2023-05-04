a387ec3b528293e86b7d2100d274855b65ab1690
Bhavana <Bhavana@192.168.1.2>
2023-05-04 04:50:43 -0500

    second prg

---
+++ B_54/src/com/chardemo.java
@@ -0,0 +1,22 @@
+package com;
+
+public class chardemo {
+	public static void main(String[] args) {
+		//assigning single charter literally
+		char ch = 'a';
+		System.out.println(ch);
+		
+		//assigning number to char
+		char ch1 = 65;
+		System.out.println(ch1);
+		
+		//assigning unicode to char
+		char var1 = '\u00a7';
+		System.out.println(var1);
+		
+		//asci code represetation
+		byte a = 'A';
+		System.out.println(a);
+	}
+
+}
